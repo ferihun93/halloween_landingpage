d56abe0e995516be6c9e2a1a5f02459af60c1a6a
ferihun93 <ferenc.olah1993@gmail.com>
2024-11-30 14:26:56 +0100

    kodfeljavitas és footer

--- backtotop.js
+++ backtotop.js
@@ -1,18 +1,15 @@
-let mybutton = document.getElementById("backtotopicon");
+const mybutton = document.getElementById("backtotopicon");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// Show the button when the user scrolls down 20px from the top of the document
+window.onscroll = () => {
+  mybutton.style.display =
+    document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+      ? "block"
+      : "none";
+};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+// Scroll to the top of the document when the button is clicked
+mybutton.addEventListener("click", () => {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
-}
+});
